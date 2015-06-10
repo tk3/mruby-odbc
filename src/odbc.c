1e95aa4f06e896d155094e79d812611972fe5714
qtakamitsu <qtakamitsu@gmail.com>
2015-06-10 15:19:37 +0000

    Add ODBC::Stmt#last_error

--- src/odbc.c
+++ src/odbc.c
@@ -47,6 +47,7 @@ static mrb_value mrb_odbc_stmt_initialize(mrb_state *mrb, mrb_value self);
 static mrb_value mrb_odbc_stmt_exec_direct(mrb_state *mrb, mrb_value self);
 static mrb_value mrb_odbc_stmt_num_result_cols(mrb_state *mrb, mrb_value self);
 static mrb_value mrb_odbc_stmt_row_count(mrb_state *mrb, mrb_value self);
+static mrb_value mrb_odbc_stmt_last_error(mrb_state *mrb, mrb_value self);
 static void mrb_odbc_stmt_free(mrb_state *mrb, void *p);
 
 /* ODBC::ResultSet */
@@ -310,6 +311,36 @@ static mrb_value mrb_odbc_stmt_row_count(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(rows);
 }
 
+static mrb_value mrb_odbc_stmt_last_error(mrb_state *mrb, mrb_value self)
+{
+  SQLRETURN r;
+  mrb_odbc_stmt *stmt;
+  SQLCHAR state[SQL_SQLSTATE_SIZE];
+  SQLINTEGER native_error;
+  SQLCHAR message[256];
+  SQLSMALLINT message_len;
+  struct RClass *class_odbc;
+  struct RClass *class_odbc_error;
+  mrb_value c;
+
+  stmt = mrb_get_datatype(mrb, self, &mrb_odbc_stmt_type);
+
+  r = SQLGetDiagRec(SQL_HANDLE_STMT, stmt->stmt, 1, state, &native_error, message, sizeof(message), &message_len);
+  if (!SQL_SUCCEEDED(r)) {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "Failed to num result cols");
+  }
+
+  class_odbc = mrb_module_get(mrb, "ODBC");
+  class_odbc_error = mrb_class_ptr(mrb_const_get(mrb, mrb_obj_value(class_odbc), mrb_intern_lit(mrb, "Error")));
+  c = mrb_class_new_instance(mrb, 0, NULL, class_odbc_error);
+
+  mrb_iv_set(mrb, c, mrb_intern_cstr(mrb, "state"), mrb_str_new(mrb, (char *)state, sizeof(state)));
+  mrb_iv_set(mrb, c, mrb_intern_cstr(mrb, "native_error"), mrb_fixnum_value(native_error));
+  mrb_iv_set(mrb, c, mrb_intern_cstr(mrb, "message"), mrb_str_new_cstr(mrb, (char *)message));
+
+  return c;
+}
+
 static void mrb_odbc_stmt_free(mrb_state *mrb, void *p)
 {
   mrb_odbc_stmt *stmt = (mrb_odbc_stmt *)p;
@@ -540,6 +571,7 @@ mrb_mruby_odbc_gem_init(mrb_state* mrb)
   mrb_define_method(mrb, class_stmt, "exec_direct", mrb_odbc_stmt_exec_direct, MRB_ARGS_REQ(1));
   mrb_define_method(mrb, class_stmt, "num_result_cols", mrb_odbc_stmt_num_result_cols, MRB_ARGS_NONE());
   mrb_define_method(mrb, class_stmt, "row_count", mrb_odbc_stmt_row_count, MRB_ARGS_NONE());
+  mrb_define_method(mrb, class_stmt, "last_error", mrb_odbc_stmt_last_error, MRB_ARGS_NONE());
 
   /* ODBC::ResultSet: methods */
   class_resultset = mrb_define_class_under(mrb, module_odbc, "ResultSet", mrb->object_class);
