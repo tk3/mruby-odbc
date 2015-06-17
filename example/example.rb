395e69c022b7e1685ec420d1161675cc9bd93355
qtakamitsu <qtakamitsu@gmail.com>
2015-06-17 13:50:36 +0000

    Add example.eb

---
+++ example/example.rb
@@ -0,0 +1,33 @@
+env = ODBC::Env.new
+env.set_attr ODBC::Env::ODBC_VERSION, ODBC::Env::VERSION_3
+
+conn = ODBC::Conn.new env
+conn.connect 'DSN=myodbc'
+
+stmt = ODBC::Stmt.new conn
+stmt.execute 'DROP TABLE IF EXISTS foo'
+stmt.execute <<END_OF_SQL
+CREATE TABLE foo (
+  id int,
+  name text
+)
+END_OF_SQL
+
+stmt = ODBC::Stmt.new conn
+stmt.execute "insert into foo values(1, 'aaa')"
+stmt.execute "insert into foo values(2, 'bbb')"
+stmt.execute "insert into foo values(3, 'ccc')"
+stmt.execute "insert into foo values(4, 'ddd')"
+stmt.execute "insert into foo values(5, 'eee')"
+
+rs = stmt.execute 'SELECT * FROM foo'
+rs.each do |row|
+  d = []
+  rs.num_result_cols.times do |i|
+    d << row[i + 1]
+  end
+  puts d.join(',')
+end
+
+conn.close
+env.close
